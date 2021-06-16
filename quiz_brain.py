def6018c3d757248dd94f97ad96fa78dc489db0c
Sharaar <walidsharar@gmail.com>
2021-06-16 15:41:13 +0200

    check the avialable questions

---
+++ quiz_brain.py
@@ -0,0 +1,18 @@
+class QuizBrain:
+    def __init__(self,q_list):
+        self.question_number=0
+        self.question_list=q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number +=1
+        input(f"Question. {self.question_number} : {current_question.text} (True/False)")
+
+    def still_has_question(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+            return False
+
+
+
